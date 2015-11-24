8c8269b3629a0f71c7a9dfdcb510fc175cefb7f7
kc merrill <casey.merrill@returnpath.net>
2015-11-24 13:15:09 -0700

    Revert "Made docker host IP dynamic"
    This reverts commit 7c08ca98c97fec517e6263055a5936abd8f40159.

--- bin/automagicproxy.php
+++ bin/automagicproxy.php
@@ -13,12 +13,6 @@
 $timezone = getenv('AUTOMAGIC_TIMEZONE') ? getenv('AUTOMAGIC_TIMEZONE') : 'America/Denver';
 date_default_timezone_set($timezone);
 
-// Figure out the correct IP subnet based on container's IP
-$localIP = getHostByName(getHostName());
-$parts = explode('.', $localIP);
-$parts[3] = 1;
-$hostIP = implode('.', $parts);
-
 $timeout = getenv('AUTOMAGIC_TIMEOUT') ? getenv('AUTOMAGIC_TIMEOUT') : 10000;
 
 _log('Loading templates');
@@ -51,7 +45,7 @@ if($containers = getContainers()){
             if(isset($c_port['PrivatePort']) && in_array($c_port['PrivatePort'], $ports)){
                 if(isset($c_port['PublicPort'])){
                     $web_containers[$c_name] = $c_port['PublicPort'];
-                    _log($c_name . '* -> ' . $hostIP . ':' . $c_port['PublicPort'], 'WEB');
+                    _log($c_name . '* -> 172.17.42.1:' . $c_port['PublicPort'], 'WEB');
                 }
             }
         }
@@ -74,7 +68,7 @@ if($containers = getContainers()){
                         list($incoming, $container) = explode(':', $map);
                         if(isset($web_containers[$container])){
                             $web_containers[$incoming] = $web_containers[$container];
-                            _log($incoming . '* -> ' . $hostIP . ':' . $web_containers[$container], 'WEB');
+                            _log($incoming . '* -> 172.17.42.1:' . $web_containers[$container], 'WEB');
                         }
                     }
                 }
@@ -94,7 +88,6 @@ if($containers = getContainers()){
         $frontend .= str_replace('__host__', $wc_host, $t_frontend);
         $be_temp  = str_replace('__port__', $wc_port, $t_backend);
         $be_temp  = str_replace('__host__', $wc_host, $be_temp);
-        $be_temp  = str_replace('__hostip__', $hostIP, $be_temp);
         $backend .= $be_temp;
     }
 
@@ -112,9 +105,7 @@ if($containers = getContainers()){
 
 /* Quick functions */
 function getContainers(){
-    global $hostIP;
-
-    $api = 'http://' . $hostIP . ':2375/containers/json';
+    $api = 'http://172.17.42.1:2375/containers/json';
     /* Debugging with boot2docker */
     //$api = 'http://192.168.59.103:2375/containers/json';
     _log('Fetching containers using Docker remote API: ' . $api );
